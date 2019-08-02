8c6532c8f1ff2b6e553007f226b20e428c3d5008
lma <lma@crehler.com>
2019-08-02 14:37:52 +0200

    Add comments documentor

--- ShopwareApiBundle/Controller/ShopwareApiInterface.php
+++ ShopwareApiBundle/Controller/ShopwareApiInterface.php
@@ -5,8 +5,29 @@ namespace App\LmaDev\ShopwareApiBundle\Controller;
 
 interface ShopwareApiInterface
 {
+    /**
+     * @param String $action
+     * @return mixed
+     */
     public function get(String $action);
+
+    /**
+     * @param String $action
+     * @return mixed
+     */
     public function post(String $action);
+
+    /**
+     * @param String $action
+     * @param int $productID
+     * @return mixed
+     */
     public function put(String $action, int $productID);
+
+    /**
+     * @param String $action
+     * @param int $productID
+     * @return mixed
+     */
     public function delete(String $action, int $productID);
 }
